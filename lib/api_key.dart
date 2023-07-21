36d820f9de6a4dd31c7326afe2951d5ea2a14f5e
theakanfecode1 <danielogundiranakanfe@gmail.com>
2023-07-21 07:47:16 +0100

    feat: added NASA api key

---
+++ lib/api_key.dart
@@ -0,0 +1,9 @@
+/*
+API_KEY to access NASA API. When deploying to store obfuscation
+will be enabled making it almost for hackers to break into the app
+ */
+
+const String nasaApiKey = 'RCVnyqgU3ebcTBwsOkjpDQJrsjGZ7igLRZIcMENx';
+
+
+
