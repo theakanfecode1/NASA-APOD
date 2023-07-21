cd2c1b7a4c35dd4c861c3b1f2b169854aee13fd1
theakanfecode1 <danielogundiranakanfe@gmail.com>
2023-07-21 08:16:25 +0100

    feat: Added APOD Repository

---
+++ lib/repository/apod_repository.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nasa_apod/datasource/apod_datasource.dart';
+import 'package:nasa_apod/models/apod.dart';
+
+
+final apodRepositoryProvider =
+Provider<ApodRepository>((ref) => ApodRepository());
+
+class ApodRepository {
+
+  Future<List<Apod>> getApods({required int pageNum}) {
+    return ApodDataSource.getApods(pageNum: pageNum);
+  }
+
+}
