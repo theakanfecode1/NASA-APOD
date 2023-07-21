4cc3a88d6a27814e9a2310441f09bceb55bda17d
theakanfecode1 <danielogundiranakanfe@gmail.com>
2023-07-21 07:44:34 +0100

    feat: added app styling

---
+++ lib/res/style/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color blue = Color(0xFF141625);
+  static const Color blueLight = Color(0xFF1E2139);
+  static const Color black = Color(0xFF0C0E16);
+  static const Color white = Color(0xFFF8F8FB);
+}
---
+++ lib/res/style/app_text_styles.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:nasa_apod/res/style/app_colors.dart';
+
+class AppTextStyles {
+  static const kH1Light = TextStyle(
+    letterSpacing: -1,
+    fontSize: 32.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const kH2Light = TextStyle(
+    letterSpacing: -0.63,
+    fontSize: 22.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const kH3Lightx2 = TextStyle(
+    letterSpacing: -0.8,
+    fontSize: 16.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const kH3Lightx1 = TextStyle(
+    letterSpacing: -0.25,
+    fontSize: 12.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const kB1 = TextStyle(
+    letterSpacing: -0.25,
+    fontSize: 15.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+
+  static const kB2 = TextStyle(
+    letterSpacing: -0.23,
+    fontSize: 11.0,
+    color: AppColors.white,
+    fontWeight: FontWeight.w500,
+  );
+}
