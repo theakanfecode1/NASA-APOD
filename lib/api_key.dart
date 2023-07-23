9af7f8cc907084818831efaad5aede02ea63342a
theakanfecode1 <danielogundiranakanfe@gmail.com>
2023-07-23 18:47:38 +0100

    chore: Updated comments on API KEY

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,6 +1,6 @@
 /*
 API_KEY to access NASA API. When deploying to store obfuscation
-will be enabled making it almost for hackers to break into the app
+will be enabled making it almost impossible for hackers to break into the app
  */
 
 const String nasaApiKey = 'RCVnyqgU3ebcTBwsOkjpDQJrsjGZ7igLRZIcMENx';
