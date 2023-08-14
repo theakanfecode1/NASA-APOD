293f8b2d451a50cde4d33fe1b604fd525f1e4ef9
Daniel Ogundiran <56872396+theakanfecode1@users.noreply.github.com>
2023-08-14 14:47:25 +0100

    Update api_key.dart

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,9 +1,4 @@
-/*
-API_KEY to access NASA API. When deploying to store obfuscation
-will be enabled making it almost impossible for hackers to break into the app
- */
-
-const String nasaApiKey = 'RCVnyqgU3ebcTBwsOkjpDQJrsjGZ7igLRZIcMENx';
+const String nasaApiKey = 'inser your api key';
 
 
 
